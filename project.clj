9513423f923c728d55305aa33af9f8b67a739d64
Sam Ritchie <sritchie@twitter.com>
2011-09-01 15:27:42 -0700

    bumped version.

--- project.clj
+++ project.clj
@@ -1,11 +1,8 @@
-(defproject backtype/dfs-datastores "1.0.3"
+(defproject backtype/dfs-datastores "1.0.4"
   :java-source-path "src/jvm"
+  :java-test-path "test/jvm"
   :javac-options {:debug "true" :fork "true"}
-  :dependencies [
-                 [jvyaml "1.0.0"]
-                 [com.google.guava/guava "r09"]
-                 ]
-  :dev-dependencies [
-                     [org.apache.hadoop/hadoop-core "0.20.2-dev"]
-                     [junit/junit "3.8.2"]
-                    ])
+  :dependencies [[jvyaml "1.0.0"]
+                 [com.google.guava/guava "r09"]]
+  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
+                     [junit/junit "3.8.2"]])
