d646c836b81f3bbd62772730b2244a1e06ed8bdf
Soren Macbeth <soren@dopeness.org>
2012-01-20 14:02:04 -0800

    prep for clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject backtype/dfs-datastores "1.1.1-SNAPSHOT"
+(defproject org.clojars.sorenmacbeth/dfs-datastores "1.1.1-SNAPSHOT"
   :source-path "src/clj"
   :test-path "test/clj"
   :java-source-path "src/jvm"
