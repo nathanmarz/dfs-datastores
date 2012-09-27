2c5b07c96fadd4006805f48c08aa5b6e72ab1110
Sam Ritchie <sritchie@twitter.com>
2012-09-27 16:06:37 -0700

    bump guava.

--- project.clj
+++ project.clj
@@ -1,18 +1,16 @@
-(defproject backtype/dfs-datastores "1.1.4-SNAPSHOT"
-  :source-path "src/clj"
-  :test-path "test/clj"
-  :java-source-path "src/jvm"
-  :java-test-path "test/jvm"
-  :javac-options {:debug "true" :fork "true"}
-  :javac-source-path [["src"] ["test"]]
-  :junit [["classes"]]
+(defproject backtype/dfs-datastores "1.2.0-SNAPSHOT"
+  :min-lein-version "2.0.0"
+  :source-paths ["src/clj"]
+  :test-paths ["test/clj"]
+  :java-source-paths ["src/jvm" "test/jvm"]
+  :hooks [leiningen.hooks.junit]
+  :junit ["test/jvm"]
   :junit-options {:fork "off" :haltonfailure "on"}
   :dependencies [[jvyaml "1.0.0"]
-                 [com.google.guava/guava "r09"]]
-  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
-                     [midje "1.3.0" :exclusions [org.clore/clojure]]
-                     [org.clojure/clojure "1.2.1"]
-                     [lein-midje "1.0.8"]
-                     [lein-javac "1.3.0"]
-                     [lein-junit "1.0.0"]
-                     [junit "4.7"]])
+                 [com.google.guava/guava "13.0"]]
+  :plugins [[lein-midje "1.0.10"]
+            [lein-junit "1.0.3"]]
+  :profiles {:dev {:dependencies [[junit "4.10"]
+                                  [org.apache.hadoop/hadoop-core "0.20.2-dev"]
+                                  [midje "1.4.0" :exclusions [org.clojure/clojure]]
+                                  [org.clojure/clojure "1.4.0"]]}})
