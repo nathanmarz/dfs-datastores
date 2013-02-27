71ae7ed8445aa13c2b6d2d070d8e92b317699078
Derrick Burns <derrick.burns@rincaro.com>
2013-02-26 16:21:23 -0800

    Use nathanmarz:dfs-datastores/develop version of PailTap.

--- dfs-datastores-cascading/src/main/java/com/backtype/cascading/tap/PailTap.java
+++ dfs-datastores-cascading/src/main/java/com/backtype/cascading/tap/PailTap.java
@@ -2,7 +2,6 @@ package com.backtype.cascading.tap;
 
 import java.io.IOException;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -40,313 +39,276 @@ import cascading.tuple.TupleEntry;
 import cascading.tuple.hadoop.TupleSerialization;
 
 public class PailTap extends Hfs {
-    volatile private static Logger LOG = Logger.getLogger(PailTap.class);
+  private static Logger LOG = Logger.getLogger(PailTap.class);
 
-    public static PailSpec makeSpec(PailSpec given, PailStructure structure) {
-        return (given==null)
-            ? PailFormatFactory.getDefaultCopy().setStructure(structure)
-            : given.setStructure(structure);
-    }
+  public static PailSpec makeSpec(PailSpec given, PailStructure structure) {
+    return (given == null) ? PailFormatFactory.getDefaultCopy().setStructure(structure) :
+        given.setStructure(structure);
+  }
 
-    public static class PailTapOptions implements Serializable {
-        public PailSpec spec = null;
-        public String fieldName = "bytes";
-        public List<String>[] attrs = null;
-        public PailPathLister lister = null;
+  public static class PailTapOptions implements Serializable {
+    public PailSpec spec = null;
+    public String fieldName = "bytes";
+    public List<String>[] attrs = null;
+    public PailPathLister lister = null;
 
-        public PailTapOptions() {
-
-        }
-        
-        public PailTapOptions(PailSpec spec) {
-            this.spec = spec;
-        }
-        
-        public PailTapOptions(PailSpec spec, List<String>[] attrs ) {
-            this.spec = spec;
-            this.attrs = attrs;
-        }
-
-        public PailTapOptions(PailSpec spec, String fieldName, List<String>[] attrs, PailPathLister lister) {
-            this.spec = spec;
-            this.fieldName = fieldName;
-            this.attrs = attrs;
-            this.lister = lister;
-        }
-
-        public PailSpec getSpec() {
-            return spec;
-        }
-
-        public String getFieldName() {
-            return fieldName;
-        }
-
-        public List<String>[] getAttrs() {
-            return attrs;
-        }
-
-        public PailPathLister getLister() {
-            return lister;
-        }
-    }
-
-
-    public class PailScheme extends Scheme<JobConf, RecordReader, OutputCollector, Object[], Object[]> {
-        private PailTapOptions _options;
-
-        public PailScheme(PailTapOptions options) {
-            super(new Fields("pail_root", options.fieldName), Fields.ALL);
-            _options = options;
-        }
-
-        public PailSpec getSpec() {
-            return _options.spec;
-        }
-
-        private transient BytesWritable bw;
-        private transient Text keyW;
-
-        protected Object deserialize(BytesWritable record) {
-            PailStructure structure = getStructure();
-            if(structure instanceof BinaryPailStructure) {
-                return record;
-            } else {
-                return structure.deserialize(Utils.getBytes(record));
-            }
-        }
-
-        protected void serialize(Object obj, BytesWritable ret) {
-            if(obj instanceof BytesWritable) {
-                ret.set((BytesWritable) obj);
-            } else {
-                byte[] b = getStructure().serialize(obj);
-                ret.set(b, 0, b.length);
-            }
-        }
-
-        private transient PailStructure _structure;
-
-        public PailStructure getStructure() {
-            if(_structure==null) {
-                if(getSpec()==null) {
-                    _structure = PailFormatFactory.getDefaultCopy().getStructure();
-                } else {
-                    _structure = getSpec().getStructure();
-                }
-            }
-            return _structure;
-        }
-
-        @Override
-        public void sourceConfInit(FlowProcess<JobConf> process, Tap<JobConf, RecordReader, OutputCollector> tap, JobConf conf) {
-            Pail p;
-            try {
-                p = new Pail(_pailRoot); //make sure it exists
-            } catch (IOException e) {
-                throw new TapException(e);
-            }
-            conf.setInputFormat(p.getFormat().getInputFormatClass());
-            PailFormatFactory.setPailPathLister(conf, _options.lister);
-        }
-
-        @Override public void sinkConfInit(FlowProcess<JobConf> flowProcess,
-            Tap<JobConf, RecordReader, OutputCollector> tap, JobConf conf) {
-            conf.setOutputFormat(PailOutputFormat.class);
-            Utils.setObject(conf, PailOutputFormat.SPEC_ARG, getSpec());
-            try {
-                Pail.create(getFileSystem(conf), _pailRoot, getSpec(), true);
-            } catch (IOException e) {
-                throw new TapException(e);
-            }
-        }
-
-        @Override
-        public void sourcePrepare(FlowProcess<JobConf> flowProcess, SourceCall<Object[], RecordReader> sourceCall) {
-            sourceCall.setContext(new Object[2]);
-
-            sourceCall.getContext()[0] = sourceCall.getInput().createKey();
-            sourceCall.getContext()[1] = sourceCall.getInput().createValue();
-        }
-
-        @Override
-        public boolean source(FlowProcess<JobConf> process, SourceCall<Object[], RecordReader> sourceCall) throws IOException {
-            Object k = sourceCall.getContext()[0];
-            Object v = sourceCall.getContext()[1];
-            boolean result = sourceCall.getInput().next(k, v);
-            if(!result) return false;
-            String relPath = ((Text) k).toString();
-            Object value = deserialize((BytesWritable) v);
-            sourceCall.getIncomingEntry().setTuple(new Tuple(relPath, value));
-            return true;
-        }
-
-        @Override
-        public void sink(FlowProcess<JobConf> process, SinkCall<Object[], OutputCollector> sinkCall) throws IOException {
-            TupleEntry tuple = sinkCall.getOutgoingEntry();
-
-            Object obj = tuple.getObject(0);
-            String key;
-            //a hack since byte[] isn't natively handled by hadoop
-            key = getKey(obj);
-            if(bw==null) bw = new BytesWritable();
-            if(keyW==null) keyW = new Text();
-            serialize(obj, bw);
-            keyW.set(key);
-            sinkCall.getOutput().collect(keyW, bw);
-        }
-
-        private String getKey(Object obj) {
-            String key;
-            if(getStructure() instanceof DefaultPailStructure) {
-                key = getCategory(obj);
-            } else {
-                key = Utils.join(getStructure().getTarget(obj), "/") + getCategory(obj);
-            }
-            return key;
-        }
+    public PailTapOptions() {
 
     }
 
-    protected String _pailRoot;
+    public PailTapOptions(PailSpec spec, String fieldName, List<String>[] attrs,
+        PailPathLister lister) {
+      this.spec = spec;
+      this.fieldName = fieldName;
+      this.attrs = attrs;
+      this.lister = lister;
+    }
+  }
+
+  public class PailScheme
+      extends Scheme<JobConf, RecordReader, OutputCollector, Object[], Object[]> {
     private PailTapOptions _options;
 
-    protected String getCategory(Object obj) {
-        return "";
+    public PailScheme(PailTapOptions options) {
+      super(new Fields("pail_root", options.fieldName), Fields.ALL);
+      _options = options;
     }
 
-    public PailTap(String root, PailTapOptions options) {
-        _options = options;
-        setStringPath(root);
-        setScheme(new PailScheme(options));
-        _pailRoot = root;
+    public PailSpec getSpec() {
+      return _options.spec;
     }
-    
-    public List<Path> getPaths() {
-        final List<Path> paths = new ArrayList<Path>();
-        if(_options.attrs!=null && _options.attrs.length>0) {
-            for(List<String> attr: _options.attrs) {
-                String rel = Utils.join(attr, "/");
-                paths.add(new Path( _pailRoot, rel));
-            }
+
+    private transient BytesWritable bw;
+    private transient Text keyW;
+
+    protected Object deserialize(BytesWritable record) {
+      PailStructure structure = getStructure();
+      if (structure instanceof BinaryPailStructure) {
+        return record;
+      } else {
+        return structure.deserialize(Utils.getBytes(record));
+      }
+    }
+
+    protected void serialize(Object obj, BytesWritable ret) {
+      if (obj instanceof BytesWritable) {
+        ret.set((BytesWritable) obj);
+      } else {
+        byte[] b = getStructure().serialize(obj);
+        ret.set(b, 0, b.length);
+      }
+    }
+
+    private transient PailStructure _structure;
+
+    public PailStructure getStructure() {
+      if (_structure == null) {
+        if (getSpec() == null) {
+          _structure = PailFormatFactory.getDefaultCopy().getStructure();
         } else {
-            paths.add(new Path(_pailRoot));
+          _structure = getSpec().getStructure();
         }
-        return paths;
-    }
-
-    public PailTap(String root) {
-        this(root, new PailTapOptions());
+      }
+      return _structure;
     }
 
     @Override
-    public boolean deleteResource(JobConf conf) throws IOException {
-        throw new UnsupportedOperationException();
+    public void sourceConfInit(FlowProcess<JobConf> process,
+        Tap<JobConf, RecordReader, OutputCollector> tap, JobConf conf) {
+      Pail p;
+      try {
+        p = new Pail(_pailRoot); //make sure it exists
+      } catch (IOException e) {
+        throw new TapException(e);
+      }
+      conf.setInputFormat(p.getFormat().getInputFormatClass());
+      PailFormatFactory.setPailPathLister(conf, _options.lister);
     }
 
-
-    //no good way to override this, just had to copy/paste and modify
-    @Override
-    public void sourceConfInit(FlowProcess<JobConf> process, JobConf conf) {
-        try {
-            Path root = getQualifiedPath(conf);
-            if(_options.attrs!=null && _options.attrs.length>0) {
-                Pail pail = new Pail(_pailRoot);
-                for(List<String> attr: _options.attrs) {
-                    String rel = Utils.join(attr, "/");
-                    pail.getSubPail(rel); //ensure the path exists
-                    Path toAdd = new Path(root, rel);
-                    LOG.info("Adding input path " + toAdd.toString());
-                    FileInputFormat.addInputPath(conf, toAdd);
-                }
-            } else {
-                FileInputFormat.addInputPath(conf, root);
-            }
-
-            getScheme().sourceConfInit(process, this, conf);
-            makeLocal( conf, getQualifiedPath(conf), "forcing job to local mode, via source: " );
-            TupleSerialization.setSerializations( conf );
-        } catch(IOException e) {
-            throw new TapException(e);
-        }
-    }
-
-    private void makeLocal(JobConf conf, Path qualifiedPath, String infoMessage) {
-        if( !conf.get( "mapred.job.tracker", "" ).equalsIgnoreCase( "local" ) &&
-            qualifiedPath.toUri().getScheme().equalsIgnoreCase( "file" ) )
-        {
-            if( LOG.isInfoEnabled() )
-                LOG.info( infoMessage + toString() );
-
-            conf.set( "mapred.job.tracker", "local" ); // force job to run locally
-        }
+    @Override public void sinkConfInit(FlowProcess<JobConf> flowProcess,
+        Tap<JobConf, RecordReader, OutputCollector> tap, JobConf conf) {
+      conf.setOutputFormat(PailOutputFormat.class);
+      Utils.setObject(conf, PailOutputFormat.SPEC_ARG, getSpec());
+      try {
+        Pail.create(getFileSystem(conf), _pailRoot, getSpec(), true);
+      } catch (IOException e) {
+        throw new TapException(e);
+      }
     }
 
     @Override
-    public void sinkConfInit(FlowProcess<JobConf> process, JobConf conf) {
-        if(_options.attrs!=null && _options.attrs.length > 0)
-            throw new TapException("can't declare attributes in a sink");
+    public void sourcePrepare(FlowProcess<JobConf> flowProcess,
+        SourceCall<Object[], RecordReader> sourceCall) {
+      sourceCall.setContext(new Object[2]);
 
-        super.sinkConfInit(process, conf);
+      sourceCall.getContext()[0] = sourceCall.getInput().createKey();
+      sourceCall.getContext()[1] = sourceCall.getInput().createValue();
     }
 
     @Override
-    public boolean commitResource(JobConf conf) throws IOException {
-        Pail p = Pail.create(_pailRoot, ((PailScheme)getScheme()).getSpec(), false);
-        FileSystem fs = p.getFileSystem();
-        Path tmpPath = new Path(_pailRoot, "_temporary");
-        if(fs.exists(tmpPath)) {
-            LOG.info("Deleting _temporary directory left by Hadoop job: " + tmpPath.toString());
-            fs.delete(tmpPath, true);
-        }
-
-        Path tmp2Path = new Path(_pailRoot, "_temporary2");
-        if(fs.exists(tmp2Path)) {
-            LOG.info("Deleting _temporary2 directory: " + tmp2Path.toString());
-            fs.delete(tmp2Path, true);
-        }
-
-        Path logPath = new Path(_pailRoot, "_logs");
-        if(fs.exists(logPath)) {
-            LOG.info("Deleting _logs directory left by Hadoop job: " + logPath.toString());
-            fs.delete(logPath, true);
-        }
-
-        return true;
+    public boolean source(FlowProcess<JobConf> process,
+        SourceCall<Object[], RecordReader> sourceCall) throws IOException {
+      Object k = sourceCall.getContext()[0];
+      Object v = sourceCall.getContext()[1];
+      boolean result = sourceCall.getInput().next(k, v);
+      if (!result) { return false; }
+      String relPath = ((Text) k).toString();
+      Object value = deserialize((BytesWritable) v);
+      sourceCall.getIncomingEntry().setTuple(new Tuple(relPath, value));
+      return true;
     }
 
     @Override
-    public int hashCode() {
-        return _pailRoot.hashCode();
+    public void sink(FlowProcess<JobConf> process, SinkCall<Object[], OutputCollector> sinkCall)
+        throws IOException {
+      TupleEntry tuple = sinkCall.getOutgoingEntry();
+
+      Object obj = tuple.getObject(0);
+      String key;
+      //a hack since byte[] isn't natively handled by hadoop
+      if (getStructure() instanceof DefaultPailStructure) {
+        key = getCategory(obj);
+      } else {
+        key = Utils.join(getStructure().getTarget(obj), "/") + getCategory(obj);
+      }
+      if (bw == null) { bw = new BytesWritable(); }
+      if (keyW == null) { keyW = new Text(); }
+      serialize(obj, bw);
+      keyW.set(key);
+      sinkCall.getOutput().collect(keyW, bw);
     }
 
-    @Override
-    public boolean equals(Object object) {
-        if(!getClass().equals(object.getClass())) {
-            return false;
+  }
+
+  private String _pailRoot;
+  private PailTapOptions _options;
+
+  protected String getCategory(Object obj) {
+    return "";
+  }
+
+  public PailTap(String root, PailTapOptions options) {
+    _options = options;
+    setStringPath(root);
+    setScheme(new PailScheme(options));
+    _pailRoot = root;
+  }
+
+  public PailTap(String root) {
+    this(root, new PailTapOptions());
+  }
+
+  @Override
+  public String getIdentifier() {
+      if (_options.attrs != null && _options.attrs.length > 0) {
+          String rel = "";
+          for (List<String> attr : _options.attrs) {
+              rel += Utils.join(attr, Path.SEPARATOR);
+          }
+          return getPath().toString() + Path.SEPARATOR + rel;
+      } else {
+          return getPath().toString();
+      }
+  }
+
+  @Override
+  public boolean deleteResource(JobConf conf) throws IOException {
+    throw new UnsupportedOperationException();
+  }
+
+
+  //no good way to override this, just had to copy/paste and modify
+  @Override
+  public void sourceConfInit(FlowProcess<JobConf> process, JobConf conf) {
+    try {
+      Path root = getQualifiedPath(conf);
+      if (_options.attrs != null && _options.attrs.length > 0) {
+        Pail pail = new Pail(_pailRoot);
+        for (List<String> attr : _options.attrs) {
+          String rel = Utils.join(attr, "/");
+          pail.getSubPail(rel); //ensure the path exists
+          Path toAdd = new Path(root, rel);
+          LOG.info("Adding input path " + toAdd.toString());
+          FileInputFormat.addInputPath(conf, toAdd);
         }
-        PailTap other = (PailTap) object;
-        Set<List<String>> myattrs = new HashSet<List<String>>();
-        if(_options.attrs!=null) {
-            Collections.addAll(myattrs, _options.attrs);
-        }
-        Set<List<String>> otherattrs = new HashSet<List<String>>();
-        if(other._options.attrs!=null) {
-            Collections.addAll(otherattrs, other._options.attrs);
-        }
-        return _pailRoot.equals(other._pailRoot) && myattrs.equals(otherattrs);
+      } else {
+        FileInputFormat.addInputPath(conf, root);
+      }
+
+      getScheme().sourceConfInit(process, this, conf);
+      makeLocal(conf, getQualifiedPath(conf), "forcing job to local mode, via source: ");
+      TupleSerialization.setSerializations(conf);
+    } catch (IOException e) {
+      throw new TapException(e);
+    }
+  }
+
+  private void makeLocal(JobConf conf, Path qualifiedPath, String infoMessage) {
+    if (!conf.get("mapred.job.tracker", "").equalsIgnoreCase("local") && qualifiedPath.toUri()
+        .getScheme().equalsIgnoreCase("file")) {
+      if (LOG.isInfoEnabled()) { LOG.info(infoMessage + toString()); }
+
+      conf.set("mapred.job.tracker", "local"); // force job to run locally
+    }
+  }
+
+  @Override
+  public void sinkConfInit(FlowProcess<JobConf> process, JobConf conf) {
+    if (_options.attrs != null && _options.attrs.length > 0) {
+      throw new TapException("can't declare attributes in a sink");
     }
 
-    private Path getQualifiedPath(JobConf conf) throws IOException {
-        return getPath().makeQualified(getFileSystem(conf));
+    super.sinkConfInit(process, conf);
+  }
+
+  @Override
+  public boolean commitResource(JobConf conf) throws IOException {
+    Pail p = Pail.create(_pailRoot, ((PailScheme) getScheme()).getSpec(), false);
+    FileSystem fs = p.getFileSystem();
+    Path tmpPath = new Path(_pailRoot, "_temporary");
+    if (fs.exists(tmpPath)) {
+      LOG.info("Deleting _temporary directory left by Hadoop job: " + tmpPath.toString());
+      fs.delete(tmpPath, true);
     }
 
-    public String get_pailRoot() {
-        return _pailRoot;
+    Path tmp2Path = new Path(_pailRoot, "_temporary2");
+    if (fs.exists(tmp2Path)) {
+      LOG.info("Deleting _temporary2 directory: " + tmp2Path.toString());
+      fs.delete(tmp2Path, true);
     }
 
-
-    public PailTapOptions get_options() {
-        return _options;
+    Path logPath = new Path(_pailRoot, "_logs");
+    if (fs.exists(logPath)) {
+      LOG.info("Deleting _logs directory left by Hadoop job: " + logPath.toString());
+      fs.delete(logPath, true);
     }
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return _pailRoot.hashCode();
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    if (!getClass().equals(object.getClass())) {
+      return false;
+    }
+    PailTap other = (PailTap) object;
+    Set<List<String>> myattrs = new HashSet<List<String>>();
+    if (_options.attrs != null) {
+      Collections.addAll(myattrs, _options.attrs);
+    }
+    Set<List<String>> otherattrs = new HashSet<List<String>>();
+    if (other._options.attrs != null) {
+      Collections.addAll(otherattrs, other._options.attrs);
+    }
+    return _pailRoot.equals(other._pailRoot) && myattrs.equals(otherattrs);
+  }
+
+  private Path getQualifiedPath(JobConf conf) throws IOException {
+    return getPath().makeQualified(getFileSystem(conf));
+  }
 }
