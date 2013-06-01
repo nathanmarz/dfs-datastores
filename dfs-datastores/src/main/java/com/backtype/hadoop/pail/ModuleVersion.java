7f53db79b9edf377b8fa7c58d9795b2e2e0c3ff9
Derrick Burns <derrickrburns@gmail.com>
2013-05-31 23:29:27 -0700

    Added support for Jackson Fields serializer.

---
+++ dfs-datastores/src/main/java/com/backtype/hadoop/pail/ModuleVersion.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package com.backtype.hadoop.pail;
+
+import com.fasterxml.jackson.core.util.VersionUtil;
+
+public class ModuleVersion extends VersionUtil
+{
+    public final static ModuleVersion instance = new ModuleVersion();
+}
