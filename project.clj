39072307a5f9379d3590e5b8f36e8be5ed2d6352
Soren Macbeth <soren@dopeness.org>
2014-01-11 22:20:41 -0800

    use lein for building and publishing

---
+++ project.clj
@@ -0,0 +1,11 @@
+(def VERSION (slurp "VERSION"))
+(def MODULES (-> "MODULES" slurp (.split "\n")))
+(def DEPENDENCIES (for [m MODULES] [(symbol (str "com.backtype/" m)) VERSION]))
+
+(eval `(defproject com.backtype/bundle ~VERSION
+   :description "Dead-simple vertical partitioning, compression, appends, and consolidation of data on a distributed filesystem."
+   :url "https://github.com/nathanmarz/dfs-datastores"
+   :license {:name "Eclipse Public License"
+             :url "http://www.eclipse.org/legal/epl-v10.html"}
+   :plugins [[~'lein-sub "0.2.4"]]
+   :sub [~@MODULES]))
