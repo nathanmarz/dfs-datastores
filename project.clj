ad2ce6d1ef748bab8444e81fa1617ab5e8b1a43e
Soren Macbeth <soren@dopeness.org>
2014-04-02 22:24:18 -0700

    update lein-sub

--- project.clj
+++ project.clj
@@ -7,5 +7,5 @@
    :url "https://github.com/nathanmarz/dfs-datastores"
    :license {:name "Eclipse Public License"
              :url "http://www.eclipse.org/legal/epl-v10.html"}
-   :plugins [[~'lein-sub "0.2.4"]]
+   :plugins [[~'lein-sub "0.3.0"]]
    :sub [~@MODULES]))
